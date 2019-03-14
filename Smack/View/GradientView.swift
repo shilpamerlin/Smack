24f0861ae2346e1889bb349224a6687f7439ddd5
Shilpa <shilpamjoy@gmail.com>
2019-03-13 21:21:39 -0400

    first commit

---
+++ Smack/View/GradientView.swift
@@ -0,0 +1,21 @@
+//
+//  GradientView.swift
+//  Smack
+//
+//  Created by Shilpa Joy on 2019-03-10.
+//  Copyright © 2019 Shilpa Joy. All rights reserved.
+//
+
+import UIKit
+
+class GradientView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Utilities/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Shilpa Joy on 2019-03-13.
+//  Copyright © 2019 Shilpa Joy. All rights reserved.
+//
+
+import Foundation
